6469fc4154aba93ecd870995ebea958ed6aebae0
Bern <bernnystrom@gmail.com>
2021-05-07 21:11:55 +0800

    Refactor with props destructuring

--- client/src/pages/Launch.jsx
+++ client/src/pages/Launch.jsx
@@ -2,19 +2,19 @@ import { useMemo } from 'react';
 import { Appear, Button, Loading, Paragraph } from 'arwes';
 import Clickable from '../components/Clickable';
 
-const Launch = (props) => {
+const Launch = ({ planets, entered, submitLaunch, isPendingLaunch }) => {
   const selectorBody = useMemo(() => {
-    return props.planets?.map((planet) => (
+    return planets?.map((planet) => (
       <option value={planet.kepler_name} key={planet.kepler_name}>
         {planet.kepler_name}
       </option>
     ));
-  }, [props.planets]);
+  }, [planets]);
 
   const today = new Date().toISOString().split('T')[0];
 
   return (
-    <Appear id='launch' animate show={props.entered}>
+    <Appear id='launch' animate show={entered}>
       <Paragraph>
         Schedule a mission launch for interstellar travel to one of the Kepler
         Exoplanets.
@@ -32,7 +32,7 @@ const Launch = (props) => {
       </ul>
 
       <form
-        onSubmit={props.submitLaunch}
+        onSubmit={submitLaunch}
         style={{
           display: 'inline-grid',
           gridTemplateColumns: 'auto auto',
@@ -64,15 +64,15 @@ const Launch = (props) => {
         <Clickable>
           <Button
             animate
-            show={props.entered}
+            show={entered}
             type='submit'
             layer='success'
-            disabled={props.isPendingLaunch}
+            disabled={isPendingLaunch}
           >
             Launch Mission ✔
           </Button>
         </Clickable>
-        {props.isPendingLaunch && <Loading animate small />}
+        {isPendingLaunch && <Loading animate small />}
       </form>
     </Appear>
   );
