ab71f0b8206fbba6e61c80ab015f4723c77e2beb
Bern <bernnystrom@gmail.com>
2021-05-07 15:50:23 +0800

    Add header component and some funcs for success, abort and warning sound effects

--- client/src/pages/AppLayout.jsx
+++ client/src/pages/AppLayout.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Frame, withSounds, withStyles } from 'arwes';
 
+import Header from '../components/Header';
+
 const styles = () => ({
   content: {
     display: 'flex',
@@ -16,8 +18,16 @@ const styles = () => ({
   },
 });
 
-const AppLayout = (props) => {
-  return <div></div>;
+const AppLayout = ({ sounds, classes }) => {
+  const onSuccessSound = () => sounds.success && sounds.success.play();
+  const onAbortSound = () => sounds.abort && sounds.abort.play();
+  const onFailureSound = () => sounds.warning && sounds.warning.play();
+
+  return (
+    <div className={classes.content}>
+      <Header />
+    </div>
+  );
 };
 
 export default withSounds()(withStyles(styles)(AppLayout));
