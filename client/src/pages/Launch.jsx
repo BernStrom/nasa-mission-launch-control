67b4a8e20d5bce4d009c31f788bf8cb4e2daa781
Bern <bernnystrom@gmail.com>
2021-05-07 21:06:44 +0800

    Add form for inputting mission launch details

--- client/src/pages/Launch.jsx
+++ client/src/pages/Launch.jsx
@@ -11,6 +11,8 @@ const Launch = (props) => {
     ));
   }, [props.planets]);
 
+  const today = new Date().toISOString().split('T')[0];
+
   return (
     <Appear id='launch' animate show={props.entered}>
       <Paragraph>
@@ -28,6 +30,49 @@ const Launch = (props) => {
           times Earth's value
         </li>
       </ul>
+
+      <form
+        onSubmit={props.submitLaunch}
+        style={{
+          display: 'inline-grid',
+          gridTemplateColumns: 'auto auto',
+          gridGap: '10px 20px',
+        }}
+      >
+        <label htmlFor='launch-day'>Launch Date</label>
+        <input
+          type='date'
+          id='launch-day'
+          name='launch-day'
+          min={today}
+          max='2040-12-31'
+          defaultValue={today}
+        />
+        <label htmlFor='mission-name'>Mission Name</label>
+        <input type='text' id='mission-name' name='mission-name' />
+        <label htmlFor='rocket-name'>Rocket Type</label>
+        <input
+          type='text'
+          id='rocket-name'
+          name='rocket-name'
+          defaultValue='Explorer IS1'
+        />
+        <label htmlFor='planets-selector'>Destination Exoplanet</label>
+        <select id='planets-selector' name='planets-selector'>
+          {selectorBody}
+        </select>
+        <Clickable>
+          <Button
+            animate
+            show={props.entered}
+            type='submit'
+            layer='success'
+            disabled={props.isPendingLaunch}
+          >
+            Launch Mission ✔
+          </Button>
+        </Clickable>
+      </form>
     </Appear>
   );
 };
