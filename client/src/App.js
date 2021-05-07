4564adb5e5fe69171481f4af980b9dd26cad810c
Bern <bernnystrom@gmail.com>
2021-05-07 15:08:05 +0800

    Updates with main app theme, sounds effects and page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  Arwes,
+  SoundsProvider,
+  ThemeProvider,
+  createSounds,
+  createTheme,
+} from 'arwes';
+
+import AppLayout from './pages/AppLayout';
+
+import { theme, resources, sounds } from './settings';
 
 const App = () => {
   return (
-    <div></div>
+    <ThemeProvider theme={createTheme(theme)}>
+      <SoundsProvider sounds={createSounds(sounds)}>
+        <Arwes animate background={resources.background.large} pattern={resources.pattern}>
+          {anim => (
+            <Router>
+              <AppLayout show={anim.entered} />
+            </Router>
+          )}
+        </Arwes>
+      </SoundsProvider>
+    </ThemeProvider>
   );
 }
 
