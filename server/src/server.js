7929881b7df60db68cb90fb1bd2570542fbf2760
Bern <bernnystrom@gmail.com>
2021-05-08 20:37:22 +0800

    Update server to load planets data return from planets model

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,17 @@
 const http = require('http');
+
 const app = require('./app');
+const { loadPlanetsData } = require('./models/planets.model');
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+startServer();
