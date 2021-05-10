788948181623153ae4581f927d776e8415af7b59
Bern <bernnystrom@gmail.com>
2021-05-11 01:18:16 +0800

    Change kepler variable name to camelCase format

--- client/src/pages/Launch.jsx
+++ client/src/pages/Launch.jsx
@@ -5,8 +5,8 @@ import Clickable from '../components/Clickable';
 const Launch = ({ planets, entered, submitLaunch, isPendingLaunch }) => {
   const selectorBody = useMemo(() => {
     return planets?.map((planet) => (
-      <option value={planet.kepler_name} key={planet.kepler_name}>
-        {planet.kepler_name}
+      <option value={planet.keplerName} key={planet.keplerName}>
+        {planet.keplerName}
       </option>
     ));
   }, [planets]);
