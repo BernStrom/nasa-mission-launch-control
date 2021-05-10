524a6e1833a635b09ea8df0896001ea1c0bc3067
Bern <bernnystrom@gmail.com>
2021-05-11 03:05:09 +0800

    Connect launches schema to a mongoose model and update launches data model to use the 'launches' collection

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,6 @@
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
+
 const launches = new Map();
 
 let latestFlightNumber = 100;
@@ -13,14 +16,34 @@ const launch = {
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getAllLaunches() {
+  return await launchesDatabase.find({}, { _id: 0, __v: 0 });
+}
+
+async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet found');
+  }
+
+  await launchesDatabase.updateOne(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
+  );
 }
 
 function addNewLaunch(launch) {
