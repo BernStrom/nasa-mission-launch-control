ce3d624e263b407c921c2a320c7fd130da13287c
Bern <bernnystrom@gmail.com>
2021-05-11 04:09:56 +0800

    Refactor to isHabitablePlanet func for consistency in code format

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -4,14 +4,14 @@ const parse = require('csv-parse');
 
 const planets = require('./planets.mongo');
 
-const isHabitablePlanet = (planet) => {
+function isHabitablePlanet(planet) {
   return (
     planet['koi_disposition'] === 'CONFIRMED' &&
     planet['koi_insol'] > 0.36 &&
     planet['koi_insol'] < 1.11 &&
     planet['koi_prad'] < 1.6
   );
-};
+}
 
 function loadPlanetsData() {
   return new Promise((resolve, reject) => {
