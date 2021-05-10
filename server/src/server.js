b07db69e8c671a3c974e1c65eb2279eae7489446
Bern <bernnystrom@gmail.com>
2021-05-10 22:05:32 +0800

    Add setup connection to MongoDB with Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,30 @@
+require('dotenv').config();
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
-const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
+const NASA_ACCESS = process.env.NASA_ACCESS;
+const MONGO_URL = `mongodb+srv://nasa-api:${NASA_ACCESS}@nasacluster.dqhm4.mongodb.net/nasa?retryWrites=true&w=majority`;
+const server = http.createServer(app);
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
 
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
