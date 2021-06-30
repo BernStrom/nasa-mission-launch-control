12cbe6ef1b99461af12c4341a1cf2ce21795736f
Bern <bernnystrom@gmail.com>
2021-06-30 22:42:38 +0800

    :card_file_box: (build): Update mongodb url env path
    Modify url path for connection to MongoDB database to fix test build in actions workflow.

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const NASA_ACCESS = process.env.NASA_ACCESS;
-const MONGO_URL = `mongodb+srv://nasa-api:${NASA_ACCESS}@nasacluster.dqhm4.mongodb.net/nasa?retryWrites=true&w=majority`;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
